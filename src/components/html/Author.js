4a3719398a2f26d6341b790675175ecaf002eb0f
antor roy <antorroybabu@gmail.com>
2023-06-08 11:08:43 +0600

    first commit

---
+++ src/components/html/Author.js
@@ -0,0 +1,13 @@
+import "../../styles/author.css";
+
+export default function Author() {
+  return (
+    <div className="author">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="antorroy.netlify.app"
+      >{`ad<antorroy`}</a>
+    </div>
+  );
+}
